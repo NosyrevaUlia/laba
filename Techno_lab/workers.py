b40abc306b651135f813f8ca6265c554507b753c
JR <formyiyo@gmail.com>
2025-04-20 17:12:56 +0500

    users готовы

--- Techno_lab/workers.py
+++ Techno_lab/workers.py
@@ -1,26 +1,72 @@
 from datetime import datetime
+from flask import abort, make_response
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-# словарь со значениями Персонажей
 WORKERS = {
-    "Петрович": {
-        "fname": "Кобан",
-        "lname": "Петрович",
+    "Wilding": {
+        "fname": "Petrovich",
+        "lname": "Petro",
         "timestamp": get_timestamp(),
     },
-    "Просп": {
-        "fname": "Нортон",
+    "Prospector": {
+        "fname": "Norton",
         "lname": "Ruprecht",
         "timestamp": get_timestamp(),
     },
-    "Проклятая": {
-        "fname": "Патрисия",
-        "lname": "Не помню",
+    "Enchantress": {
+        "fname": "Patricia",
+        "lname": "Dorvald",
         "timestamp": get_timestamp(),
     }
 }
 
 def read_all():
     return list(WORKERS.values())
+
+def create(worker):
+    lname = worker.get("lname")
+    fname = worker.get("fname", "")
+
+    if lname and lname not in WORKERS:
+        WORKERS[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return WORKERS[lname], 201
+    else:
+        abort(
+            406,
+            f"Worker with last name {lname} already exists",
+        )
+
+def read_one(lname):
+    if lname in WORKERS:
+        return WORKERS[lname]
+    else:
+        abort(
+            404, f"Worker with last name {lname} not found"
+        )
+
+def update(lname, worker):
+    if lname in WORKERS:
+        WORKERS[lname]["fname"] = worker.get("fname", WORKERS[lname]["fname"])
+        WORKERS[lname]["timestamp"] = get_timestamp()
+        return WORKERS[lname]
+    else:
+        abort(
+            404, f"Worker with last name {lname} not found"
+        )
+
+def delete(lname):
+    if lname in WORKERS:
+        del WORKERS[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
+    else:
+        abort(
+            404, f"Worker with last name {lname} not found"
+        )
